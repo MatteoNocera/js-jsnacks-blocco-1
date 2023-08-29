e838a4ab18270d5017b9556a1164c4c9acfc436b
MatteoNocera <matteo_nocera@tiscali.it>
2023-08-29 16:51:04 +0200

    working on jsnack 3

---
+++ jsnack3/script.js
@@ -0,0 +1,22 @@
+/* 
+🍫 Snack 3:
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+
+
+
+*/
+const totalNumbers = [];
+
+for (i = 0; i <= 2; i++ ) {
+    const userNumbers = Number(prompt('insert a number between 1 and 10'));
+    //console.log(userNumbers);
+
+    totalNumbers.push(userNumbers);
+    console.log(totalNumbers);
+    
+    
+    
+
+    
+}
